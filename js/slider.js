e771aa7f0164f38f81e2c2945a011a6a937807bc
LU <monkey07233@gmail.com>
2019-05-15 13:39:39 +0800

    首頁圖片輪播

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+var total = $('.header ul li').length;
+var current = 1;
+var windows_width = screen.width;
+displaypic(current);
+function displaypic(pic) {
+    if (current < total) {
+        var left = $('.slider_header').css("margin-left");
+        $('.slider_header').animate({ "margin-left": parseInt(left) - windows_width }, 1500);
+        current = current + 1;
+        setTimeout(function () { displaypic(current) }, 3000);
+    } else {
+        $('.slider_header').animate({ "margin-left": 0 + "px" }, 1500);
+        current = 1;
+        setTimeout(function () { displaypic(current) }, 3000);
+    }
+
+}

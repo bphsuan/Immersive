6b4f08bf9d6fee4330c3308b0ae91874f44145bd
Tiffany <1102b004@gmail.com>
2019-05-18 17:52:51 +0800

    topic_content內文動畫

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
   var def_menu = $(".menu").offset().top;
   var scroll = false;
-  $(document).scroll(function () {
+  $(window).scroll(function () {
+    //往下滑動後固定左側menu
     if (document.documentElement.scrollTop >= def_menu) {
       if (!scroll) {
         $(".menu").css({ "position": "fixed", "top": "0px" });
@@ -13,6 +14,16 @@ $(document).ready(function () {
         scroll = false;
       }
     }
+    //滑到文章才顯示動畫
+    $('.topic_content').each(function (i) {
+      var bottom_of_object = $(this).offset().top+10;
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      /* If the object is completely visible in the window, fade it it */
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({ 'opacity': '1' }, 2000);
+        
+      }
+    });
   });
 });
 
@@ -34,7 +45,7 @@ $(function () {
 
 //Menu點擊變色動畫
 $(".menu ul li a").click(function () {
-  $(".menu ul li a").css({"color": "#9d9d9d"});
+  $(".menu ul li a").css({ "color": "#9d9d9d" });
   $(this).css({
     "color": "#2828ff", "animation": "color 2s",
     "-moz-animation": "color 2s", "-webkit-animation": "color 2s"

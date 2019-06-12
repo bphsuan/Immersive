b14b116f4ca14edb4b48a2ee673202aad1bbe365
LU <monkey07233@gmail.com>
2019-06-12 22:24:27 +0800

    增加議題圖片

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-var jsImg = new Array("./assets/turtle02.jpg", "./assets/turtle01.jpg", "./assets/turtle03.png", "./assets/turtle04.png");
-var jsImg_len = jsImg.length;
-var i = 0;
+// var jsImg = new Array("./assets/turtle02.jpg", "./assets/turtle01.jpg", "./assets/turtle03.png", "./assets/turtle04.png");
+// var jsImg_len = jsImg.length;
+// var i = 0;
 window.onload = function () {
   $('html,body').animate({ scrollTop: 0 }, 'slow');
 }
 
-setInterval("turtleImg()", 1500);
-function turtleImg() {
-  document.getElementById("turtlePic").innerHTML = "<img src='" + jsImg[i] + "' width=100%'>";
-  $("#turtlePic img").fadeIn(1000);
-  i++;
-  if (i >= jsImg_len) i = 0;
-}
+// setInterval("turtleImg()", 1500);
+// function turtleImg() {
+//   document.getElementById("turtlePic").innerHTML = "<img src='" + jsImg[i] + "' width=100%'>";
+//   $("#turtlePic img").fadeIn(1000);
+//   i++;
+//   if (i >= jsImg_len) i = 0;
+// }

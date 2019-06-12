e360dce0c12072d9917fab784e2a598eb217efa2
LU <monkey07233@gmail.com>
2019-06-12 22:02:32 +0800

    增加內容和footer

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 var jsImg = new Array("./assets/turtle02.jpg", "./assets/turtle01.jpg", "./assets/turtle03.png", "./assets/turtle04.png");
 var jsImg_len = jsImg.length;
 var i = 0;
+window.onload = function () {
+  $('html,body').animate({ scrollTop: 0 }, 'slow');
+}
+
 setInterval("turtleImg()", 1500);
 function turtleImg() {
   document.getElementById("turtlePic").innerHTML = "<img src='" + jsImg[i] + "' width=100%'>";

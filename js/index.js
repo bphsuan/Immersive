4b65f9f5617acde21c92d769b3a387fc78ba5604
jingyi <amychen861110@gmail.com>
2019-06-25 19:26:22 +0800

    temp

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-var jsImg = new Array("./assets/turtle02.jpg", "./assets/turtle01.jpg", "./assets/turtle03.png", "./assets/turtle04.png");
-var jsImg_len = jsImg.length;
-var i = 0;
-setInterval("turtleImg()", 1500);
-function turtleImg() {
-  document.getElementById("turtlePic").innerHTML = "<img src='" + jsImg[i] + "' width=100%'>";
-  $("#turtlePic img").fadeIn(1000);
-  i++;
-  if (i >= jsImg_len) i = 0;
-}
+// var jsImg = new Array("./assets/turtle02.jpg", "./assets/turtle01.jpg", "./assets/turtle03.png", "./assets/turtle04.png");
+// var jsImg_len = jsImg.length;
+// var i = 0;
+// setInterval("turtleImg()", 1500);
+// function turtleImg() {
+//   document.getElementById("turtlePic").innerHTML = "<img src='" + jsImg[i] + "' width=100%'>";
+//   $("#turtlePic").fadeIn(1000);
+//   i++;
+//   if (i >= jsImg_len) i = 0;
+// }

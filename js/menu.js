36bf0d490e46b52ab390255dd198c2de3fb122c7
jingyi <amychen861110@gmail.com>
2019-08-13 19:34:07 +0800

    99.5% Done

--- js/menu.js
+++ js/menu.js
@@ -105,7 +105,7 @@ $(function () {
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top + 150
+          scrollTop: target.offset().top + 20
         }, 2000);
         return false;
       }
